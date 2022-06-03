010522e095049c98c3d635d6e560d22872869277
Daniel Medina <damedina63@gmail.com>
2022-06-03 16:42:50 -0500

    finished code

---
+++ theoneportfolio/src/components/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PortfolioContainer from '../portfolio/Portfolio-Container'
+
+
+export default function home() {
+  return (
+    <div>
+    <h4>Homepage</h4>
+    <PortfolioContainer />
+    </div>
+  );
+}
